294e3521bdc8d410501954a586d5aea34704a8ca
Tomas Alvarez <tomas1alvares@gmail.com>
2022-04-05 09:20:45 -0500

    Prototipo Estructura

--- Quiz3/Program.cs
+++ Quiz3/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Aquí ya podemos pegar el codigo
+﻿namespace Quiz3
+{
+    enum States
+    {
+        INIT,
+        WAITING,
+        WRITE,
+        READ,
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            switch(States)
+
+
+            case States.INIT:
+
+                SerialPort _serialPort = new SerialPort();
+                _serialPort.PortName = "COM3";
+                _serialPort.BaudRate = 115200;
+                _serialPort.DtrEnable = true;
+                _serialPort.Open();
+
+                break;
+
+            case States.WRITE:
+
+                Serial.write(0x2A);
+                break;
+
+            case States.WAITING:
+
+                break;
+
+
+
+            case States.READ:
+
+                break;
+            
+        }
+    }
