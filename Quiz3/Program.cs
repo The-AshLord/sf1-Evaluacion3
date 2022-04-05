3d72d42ad3b82a0f453b981968a6856192f06e4c
Tomas Alvarez <tomas1alvares@gmail.com>
2022-04-05 09:23:05 -0500

    Librerias

--- Quiz3/Program.cs
+++ Quiz3/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Quiz3
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Quiz3
 {
     enum States
     {
@@ -14,33 +19,33 @@
         {
 
 
-            switch(States)
+            switch (States)
+            {
+
+                case States.INIT:
+
+                    SerialPort _serialPort = new SerialPort();
+                    _serialPort.PortName = "COM3";
+                    _serialPort.BaudRate = 115200;
+                    _serialPort.DtrEnable = true;
+                    _serialPort.Open();
+
+                    break;
+
+                case States.WRITE:
+
+                    Serial.write(0x2A);
+                    break;
+
+                case States.WAITING:
+
+                    break;
 
 
-            case States.INIT:
 
-                SerialPort _serialPort = new SerialPort();
-                _serialPort.PortName = "COM3";
-                _serialPort.BaudRate = 115200;
-                _serialPort.DtrEnable = true;
-                _serialPort.Open();
+                case States.READ:
 
-                break;
-
-            case States.WRITE:
-
-                Serial.write(0x2A);
-                break;
-
-            case States.WAITING:
-
-                break;
-
-
-
-            case States.READ:
-
-                break;
-            
+                    break;
+            }
         }
     }
